d6525c75d4933577e9c6bae471adf82afe4e3f59
Lew Goettner <goettnel@wharton.upenn.edu>
2011-04-26 08:59:37 -0400

    Project Creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Click::Application.config.secret_token = '15ebafcd791d31269a5da36900f77ef934ef4d269bf0059ea6db207479278d68cd417e32aca3311d0a03d7112fd708ec942da74f166a11a77b472aebe0cc3fb5'
