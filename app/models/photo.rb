0e2b99fb20c9f1671751a6354e18973783004262
Lew Goettner <lew@goettner.net>
2011-05-04 23:05:43 -0400

    Backtrack on exif validation

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -21,34 +21,25 @@
 class Photo < ActiveRecord::Base
   has_attached_file :image, :styles => { :display => "900x500>", :thumb => "100x100>" }
   
-  validate :check_for_exif
   validates :name, :presence => true
   
   validates_attachment_presence :image
   validates_attachment_content_type :image, :content_type => ['image/jpeg']
   
   before_save :load_exif
-  
-  def check_for_exif
-    exif_data = EXIFR::JPEG.new(self.image.to_file)
-    if exif_data.nil? or not exif_data.exif?
-      errors.add(:image, " is missing EXIF data")
-      return false
-    end
-    true
-  rescue
-    false
-  end
-  
+    
   def load_exif
     # Pull the Exif data
     exif_data = EXIFR::JPEG.new(self.image.to_file)
+    return if exif_data.nil? or not exif_data.exif?
     # Set the attributes
     self.exposure = exif_data.exposure_time.to_s
     self.f_stop = exif_data.f_number.to_f.to_s
     self.focal_length = exif_data.focal_length.to_f.round.to_s
     self.iso = exif_data.iso_speed_ratings
     self.taken_on = exif_data.date_time.to_date
+  rescue
+    false
   end
   
 end
